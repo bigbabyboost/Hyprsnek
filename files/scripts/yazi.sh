d359b4782c6e144c5e7d023e08106432d36460f5
bigbabyboost <ah0199004@gmail.com>
2025-01-04 13:24:52 +0700

    add more pkg

--- files/scripts/yazi.sh
+++ files/scripts/yazi.sh
@@ -1,7 +1,3 @@
 set -oue pipefail
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-$HOME/.cargo/bin/rustup update
-
 cargo install --locked --git https://github.com/sxyazi/yazi.git yazi-fm yazi-cli
