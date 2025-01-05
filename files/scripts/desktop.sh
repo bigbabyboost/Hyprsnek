4ed2b26353b67cd992fd998f9528eb086360dfdc
bigbabyboost <ah0199004@gmail.com>
2025-01-05 07:37:37 +0700

    fix scripts

--- files/scripts/desktop.sh
+++ files/scripts/desktop.sh
@@ -4,14 +4,12 @@ for file in /usr/share/applications/rofi.desktop \
              /usr/share/applications/rofi-theme-selector.desktop \
              /usr/share/applications/nwg-panel-config.desktop; do
     if [ -f "$file" ]; then
-        echo "Testing changes for $file:"
-        sed '/^NoDisplay=/{
+        sed -i '/^NoDisplay=/{
             s/NoDisplay=false/NoDisplay=true/
         }
         /^NoDisplay=/!{
             $a\NoDisplay=true
         }' "$file"
-        echo "--------------------------"
     else
         echo "File $file does not exist."
     fi
